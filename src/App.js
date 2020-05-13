a7b30ff388e714499d8328fc9242a85c127fc3b3
Raymond <Rayjones2170@gmail.com>
2020-05-13 14:10:59 -0400

    Adds matierals UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {currentUser:undefined};
+  }
+  
+  getCurrentUser(user){
+    console.log("Hello from App")
+    //this.setState({currentUser:user});
+  }
+  
+  render() {
+      return (
+        <div className="App">
+            <Input placeholder="Enter Standard Deviation" onChange={this.updateCode} inputProps={{ 'aria-label': 'description' }} />
+            <br></br>
+            <Input placeholder="Enter Hands Played" onChange={this.updateCode} inputProps={{ 'aria-label': 'description' }} />
+            <br></br>
+            <Input placeholder="Enter bb/100" onChange={this.updateCode} inputProps={{ 'aria-label': 'description' }} />
+            <br></br>
+
+            <Button variant="contained" color="primary">
+            Calculate
+            </Button>
+          </div>
+      );
+  }
+}
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Input placeholder="Enter Standard Deviation" onChange={this.updateCode} inputProps={{ 'aria-label': 'description' }} />
+        <Input placeholder="Enter Hands Played" onChange={this.updateCode} inputProps={{ 'aria-label': 'description' }} />
+        <Input placeholder="Enter bb/100" onChange={this.updateCode} inputProps={{ 'aria-label': 'description' }} />
+
+        <Button variant="contained" color="primary">
+         Calculate
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,5 +56,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
